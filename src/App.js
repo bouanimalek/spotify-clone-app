4916a8a352d9ad0de67cb4721b05c4bee70f2dba
malzak <malzovich@gmail.com>
2022-01-29 10:52:18 +0100

    Login component and his css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
-      <h1> hello devs 🚀 !!!</h1>
+      <Login />
     </div>
   );
 }
